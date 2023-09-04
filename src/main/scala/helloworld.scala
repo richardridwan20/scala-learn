238ea16c8cedba4ff22a0c919b395097643fb331
richardridwan20 <richardridwan20@gmail.com>
2023-09-04 12:52:10 +0700

    type interference - immutable

--- src/main/scala/helloworld.scala
+++ src/main/scala/helloworld.scala
@@ -1,3 +1,16 @@
 @main def hello(): Unit =
   println("Hello, World!")
-  println("Hello, World!")
+
+  // Calculate the difference between 8 and 5
+  val difference = 8.-(5)
+
+  // Print the difference
+  println(difference)
+
+  // Define immutable variables for player names
+  val playerA: String = "Alex"
+  val playerB: String = "Chen"
+  val playerC: String = "Umberto"
+
+  // Change playerC from Marta to Umberto
+  // playerC = "Umberto"
